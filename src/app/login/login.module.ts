97a8bab9e69500d684112a5554f5d592c2497252
Leandro Fortaleza <fortaleza555@hotmail.com>
2019-03-05 23:10:12 -0300

    repair route and fix bug keyboard

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -13,6 +13,11 @@ const routes: Routes = [
     component: LoginPage,
     children: [
       {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'signin'
+      },
+      {
         path: 'signin',
         loadChildren: '../signin/signin.module#SigninPageModule'
       },
